3f541fab3be2f5a2caf5e41cf3b53150d85f11f1
coder-nilesh <nileshkadel1@gmail.com>
2021-03-24 11:56:29 +0545

    Car game

---
+++ check_natural_numbers.py
@@ -0,0 +1,6 @@
+#check wheather 5 is in list of first 5 natural numbers or not.hint = [1,2,3,4,5]
+list =  [1,2,3,4,5]
+if 5  in list:
+    print("5 is in list")
+else:
+    print("5 is not in the list")
